4644a30f7f06d3daced02488add150e4eaea68a4
Rosculet Alin Ionut <arosculet@Rosculets-MacBook-Pro.local>
2025-03-05 13:24:35 +0200

    add navigation bar and book images

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Start of the project</h1>
-    </div>
+    <Navbar/>
   );
 }
 
