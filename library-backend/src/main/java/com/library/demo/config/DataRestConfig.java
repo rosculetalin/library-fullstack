38db1b031d1a404a596e46889e1d146f306cb2aa
Rosculet Alin Ionut <arosculet@Rosculets-MacBook-Pro.local>
2025-03-06 14:35:00 +0200

    BE add book entity configuration for allowing only read data rest operation

---
+++ library-backend/src/main/java/com/library/demo/config/DataRestConfig.java
@@ -0,0 +1,34 @@
+package com.library.demo.config;
+
+import com.library.demo.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private final String allowedOrigin = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                                                     CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.PATCH, HttpMethod.DELETE};
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, unsupportedActions);
+
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(allowedOrigin);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] unsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+    }
+}
