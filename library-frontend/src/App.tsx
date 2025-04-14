e1f7773f5640e8264ff7bb656ae7427bda04f412
Rosculet Alin Ionut <rosculet.alin.ionut@gmail.com>
2025-04-14 13:00:42 +0300

    add routing; replace anchor links with react links

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -4,14 +4,31 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div>
-        <Navbar/>
-        {/* <HomePage/> */}
-        <SearchBooksPage/>
-        <Footer/>
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+
+      <div className="flex-grow-1">
+        
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+
+          <Route path="/home">
+            <HomePage />
+          </Route>
+
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+
+      </div>
+      <Footer />
     </div>
   );
 }
