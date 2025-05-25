e6450984e91ff4d134e43b29b818d2b82a4d0b0b
Rosculet Alin Ionut <rosculet.alin.ionut@gmail.com>
2025-05-25 12:06:37 +0300

    BE add request model,service method and endpoint for adding new book as admin

---
+++ library-backend/src/main/java/com/library/demo/request_models/AddBookRequest.java
@@ -0,0 +1,19 @@
+package com.library.demo.request_models;
+
+import lombok.Data;
+
+@Data
+public class AddBookRequest {
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+    private int copies;
+
+    private String category;
+
+    private String img;
+}
