4ac5716d92f9ede541d01ca5750ce516bacb55ca
Rosculet Alin Ionut <rosculet.alin.ionut@gmail.com>
2025-05-29 14:13:28 +0300

    add self signed certificate on client; add support for https

--- library-frontend/src/okta/oktaConfig.ts
+++ library-frontend/src/okta/oktaConfig.ts
@@ -2,7 +2,7 @@
 export const oktaConfig = {
     clientId: "0oaob97s093WB4Spx5d7",
     issuer: "https://dev-66242498.okta.com/oauth2/default",
-    redirectUri: "http://localhost:3000/login/callback",
+    redirectUri: "https://localhost:3000/login/callback",
     scopes: ["openid", "profile", "email"],
     pkce: true,
     disableHttpsCheck: true
